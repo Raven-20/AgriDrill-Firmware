cd548e9dd479c5359c902739aecd3fcb631083a4
Raven-20 <sybilcelestra@gmail.com>
2026-03-05 12:35:05 +0800

    3/5/2026_feat-Esp32setup
    Initial setup for ESP32 FreeRTOS architecture

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,121 @@
 #include <Arduino.h>
+#include "config.h"
 
-void setup() {
-    Serial.begin(115200);
-    delay(1000);
-    Serial.println("AgriDrill ESP32 Boot Successful");
+// ===== TASK HANDLES =====
+TaskHandle_t MotorTask;
+TaskHandle_t SensorTask;
+TaskHandle_t ActuatorTask;
+TaskHandle_t CommunicationTask;
+TaskHandle_t SystemMonitorTask;
+
+
+// ================= MOTOR TASK =================
+void MotorControlTask(void *pvParameters)
+{
+    while (true)
+    {
+        Serial.println("Motor Control Task Running");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
-void loop() {
-    Serial.println("System Running...");
-    delay(1000);
+
+// ================= SENSOR TASK =================
+void SensorTaskFunction(void *pvParameters)
+{
+    while (true)
+    {
+        Serial.println("Sensor Task Running");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+
+// ================= ACTUATOR TASK =================
+void ActuatorTaskFunction(void *pvParameters)
+{
+    while (true)
+    {
+        Serial.println("Actuator Task Running");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+
+// ================= COMMUNICATION TASK =================
+void CommunicationTaskFunction(void *pvParameters)
+{
+    while (true)
+    {
+        Serial.println("Communication Task Running");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+
+// ================= SYSTEM MONITOR TASK =================
+void SystemMonitorTaskFunction(void *pvParameters)
+{
+    while (true)
+    {
+        Serial.println("System Monitor Running");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // ===== CREATE TASKS =====
+
+    xTaskCreatePinnedToCore(
+        MotorControlTask,
+        "MotorControl",
+        4096,
+        NULL,
+        2,
+        &MotorTask,
+        1);
+
+    xTaskCreatePinnedToCore(
+        SensorTaskFunction,
+        "SensorTask",
+        4096,
+        NULL,
+        2,
+        &SensorTask,
+        1);
+
+    xTaskCreatePinnedToCore(
+        ActuatorTaskFunction,
+        "ActuatorTask",
+        4096,
+        NULL,
+        2,
+        &ActuatorTask,
+        1);
+
+    xTaskCreatePinnedToCore(
+        CommunicationTaskFunction,
+        "CommunicationTask",
+        4096,
+        NULL,
+        1,
+        &CommunicationTask,
+        0);
+
+    xTaskCreatePinnedToCore(
+        SystemMonitorTaskFunction,
+        "SystemMonitor",
+        4096,
+        NULL,
+        1,
+        &SystemMonitorTask,
+        0);
+}
+
+void loop()
+{
 }
