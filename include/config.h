dbad8c39badb4cc2654ada7832328523c7a1ab1a
Raven-20 <sybilcelestra@gmail.com>
2026-03-12 11:46:51 +0800

    3/12/26_feat-BTS7960testing
    BTS7960 linear actuator control testing on Serial Monitor.

--- include/config.h
+++ include/config.h
@@ -1,36 +1,60 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// ===== LEFT MOTOR (BTS7960) =====
+// ==================================================
+// LEFT MOTOR (BTS7960)
+// ==================================================
 #define MOTOR_LEFT_RPWM 25
 #define MOTOR_LEFT_LPWM 13
 
-// ===== RIGHT MOTOR (BTS7960) =====
+// ==================================================
+// RIGHT MOTOR (BTS7960)
+// ==================================================
 #define MOTOR_RIGHT_RPWM 26
 #define MOTOR_RIGHT_LPWM 12
 
-// ===== ULTRASONIC SENSOR =====
+
+// ==================================================
+// ULTRASONIC SENSOR
+// ==================================================
 #define ULTRASONIC_TRIG 33
 #define ULTRASONIC_ECHO 32
 
-// ===== LINEAR ACTUATOR (BTS7960) =====
+
+// ==================================================
+// LINEAR ACTUATOR (BTS7960)
+// ==================================================
 #define ACTUATOR_RPWM 27
 #define ACTUATOR_LPWM 14
+#define ACT_EN 23
+#define LIMIT_SWITCH_PIN 34
 
-// ===== STEPPER MOTOR (ULN2003) =====
+
+// ==================================================
+// STEPPER MOTOR (ULN2003)
+// ==================================================
 #define STEPPER_IN1 18
 #define STEPPER_IN2 19
 #define STEPPER_IN3 21
 #define STEPPER_IN4 22
 
-// ===== PWM SETTINGS =====
+
+// ==================================================
+// PWM SETTINGS
+// ==================================================
 #define PWM_FREQ 20000
 #define PWM_RESOLUTION 8
 
-// ===== PWM CHANNELS =====
+
+// ==================================================
+// PWM CHANNELS
+// ==================================================
 #define LEFT_RPWM_CHANNEL 0
 #define LEFT_LPWM_CHANNEL 1
 #define RIGHT_RPWM_CHANNEL 2
 #define RIGHT_LPWM_CHANNEL 3
+#define ACT_RPWM_CHANNEL 4
+#define ACT_LPWM_CHANNEL 5
+
 
 #endif
